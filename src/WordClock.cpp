ba0bd5d453630527de6e7a46f26397a398c1f928
Diogo Rato <diogo.rato.11@gmail.com>
2019-07-09 00:58:38 +0100

    Added mapping for words

--- src/WordClock.cpp
+++ src/WordClock.cpp
@@ -1,7 +1,6 @@
 #include <WordClock.h>
 #include <WordMapping.h>
 #include <vector>
-#include <time.h>
 
 NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod>* _strip;
 WordMapping _wordMapping;
@@ -11,29 +10,65 @@ WordClock::WordClock() {}
 WordClock::WordClock(NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod>* strip) {
     _strip = strip;
     Serial.begin(115200);
-
 }
 
 RgbColor red(255, 0, 0);
 RgbColor black(0, 0, 0);
 
-void WordClock::Update() {
-    _strip->ClearTo(black);
-
-    std::vector<int> pixels;
-    std::vector<int> pixelsToAdd =
-        _wordMapping.GetPixel(WordMapping::Word::GAIPS);
-    pixels.insert(pixels.end(), pixelsToAdd.begin(), pixelsToAdd.end());
-
-    for (int i = 0; i < pixels.size(); i++) {
-        _strip->SetPixelColor(pixels[i], red);
+void FillLedsAndShow(std::vector<int> leds, RgbColor color,
+                     RgbColor background = black) {
+    _strip->ClearTo(background);
+    for (int led : leds) {
+        _strip->SetPixelColor(led, color);
     }
-
-    struct tm timeinfo;
-    if (!getLocalTime(&timeinfo)) {
-        Serial.println("Failed to obtain time");
-    }
-    Serial.println("adsasda:" + timeinfo.tm_mon);
-
     _strip->Show();
 }
+
+void WordClock::Update() {
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::GAIPS), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_EIGHT), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_ELEVEN), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_FIVE), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_FOUR), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_NINE), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_ONE), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_SEVEN), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_SIX), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_TEN), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_THREE), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_TWELVE), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::H_TWO), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::IT_S), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::M_A_QUARTER), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::M_FIVE), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::M_HALF), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::M_TEN), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::M_TWENTY), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::O_CLOCK), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::PAST), red);
+    delay(1000);
+    FillLedsAndShow(_wordMapping.GetLeds(WordMapping::Word::TO), red);
+}
+
+
--- src/WordMapping.cpp
+++ src/WordMapping.cpp
@@ -1,24 +1,20 @@
+#include <NeoPixelBus.h>
 #include <WordMapping.h>
 #include <map>
 #include <vector>
 
 std::map<WordMapping::Word, std::vector<int>> _pixelMap;
 
-WordMapping::WordMapping() {
-    std::vector<int> pixels;
-    pixels = std::vector<int>{8,9,29};
-    _pixelMap.insert(
-        std::pair<WordMapping::Word, std::vector<int>>(Word::IT_S, pixels));
-
-    pixels = std::vector<int>{60,79,80,99,100};
-    _pixelMap.insert(
-        std::pair<WordMapping::Word, std::vector<int>>(Word::GAIPS, pixels));
-
+std::vector<int> WordMapping::GetLeds(WordMapping::Word word) {
+    return WordMapping::GetLeds(std::bitset<110>(word));
 }
 
-std::vector<int> WordMapping::GetPixel(WordMapping::Word word) {
-    if(_pixelMap.find(word) != _pixelMap.end()){
-        return _pixelMap[word];
+std::vector<int> WordMapping::GetLeds(std::bitset<110> ledBitMask) {
+    std::vector<int> pixels;
+    for (int i = 0; i < 110; i++) {
+        if (ledBitMask[i] == 1) {
+            pixels.push_back(i);
+        }
     }
-    return std::vector<int>();
+    return pixels;
 }
